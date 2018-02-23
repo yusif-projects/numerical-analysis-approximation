061043a2ba7a65f8c1a54c95a80c5e2fb7f3c0a4
Yusif Aliyev <yusif.projects@gmail.com>
2018-02-24 00:35:02 +0400

    I created 2 UITextFields instead of sliders.

--- Numerical Analysis - Approximate Value/Controller/Main_VC.swift
+++ Numerical Analysis - Approximate Value/Controller/Main_VC.swift
@@ -4,12 +4,21 @@
 import UIKit
 
 class Main_VC: UIViewController {
-    @IBOutlet weak var result_label: UILabel!
+    @IBOutlet weak var power_text_box: UITextField!
+    @IBOutlet weak var accuracy_text_box: UITextField!
     
     var power: Double = 0.0
     var accuracy: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(Main_VC.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
 }
