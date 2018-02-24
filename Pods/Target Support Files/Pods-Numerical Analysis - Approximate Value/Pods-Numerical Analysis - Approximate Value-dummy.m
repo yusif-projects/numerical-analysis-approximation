1d6a149402b00c0b56a812fd4a302dfd62af30f9
Yusif Aliyev <yusif.projects@gmail.com>
2018-02-25 03:23:17 +0400

    Some details.

---
+++ Pods/Target Support Files/Pods-Numerical Analysis - Approximate Value/Pods-Numerical Analysis - Approximate Value-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Numerical_Analysis___Approximate_Value : NSObject
+@end
+@implementation PodsDummy_Pods_Numerical_Analysis___Approximate_Value
+@end
